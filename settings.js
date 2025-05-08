09181ad48286639a35ccc34d94dd2ce3dca3614d
OwnerCO <jazminqntro96@gmail.com>
2025-05-08 16:26:19 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,25 +2,25 @@ const fs = require('fs')
 const chalk = require('chalk')
 //Feel free to customize my bot informations to yours. Starting from this point
 //owmner v card
-global.ytname = "YT: ElitePro" //ur yt chanel name
-global.socialm = "GitHub: Elite-MD" //ur github or insta name
-global.location = "Port Harcourt, Nigeria" //ur location
+global.ytname = "YT: SysSolutions" //ur yt chanel name
+global.socialm = "GitHub: OwnerCO" //ur github or insta name
+global.location = "Peru,Lima" //ur location
 
 //new settings 
-global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
-global.ownername = 'ELITE-PRO-V2' //ur owner name
-global.websitex = "https://youtube.com/@elitepro-empire"
+global.botname = 'SYSSOLUTIONS-PRO-V2' //ur bot name
+global.ownernumber = '51960285930' //ur owner number
+global.ownername = 'SYSSOLUTIONS-PRO-V2' //ur owner name
+global.websitex = "https://youtube.com/@syssoluyionsperu"
 global.elitepropp = 'https://i.ibb.co/jk0ynvbn/7a8c4b5b617fa11a1e9a61190f427546.jpg' // Change this image link to yours.
 
 global.themeemoji = '👨🏼‍💻'
-global.wm = "ELITE-PRO-V2"
-global.botscript = 'https://github.com/Elite-MD/Elite-Pro-V2' //script link
-global.packname = "Sticker By"
-global.author = "ELITE-PRO-V2\n\n+2348109263390"
-global.creator = "2348109263390@s.whatsapp.net"
+global.wm = "SYSSOLUTIONS-PRO-V2"
+global.botscript = 'https://github.com/OwnerCO/Elite-Pro-V2' //script link
+global.packname = "SYSSOLUTIONS-PRO-V2"
+global.author = "SYSSOLUTIONS-PRO-V2\n\n+51960285930"
+global.creator = "51960285930@s.whatsapp.net"
 global.xprefix = '.' //set any prefix 
-global.premium = ["2348109263390"] // Premium User
+global.premium = ["51960285930"] // Premium User
 //Stop here. Thanks for choosing ELITEPRO.
 
 
@@ -30,11 +30,11 @@ global.typemenu = 'v1'
 global.typereply = 'v4' 
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
-global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.welcome = true //welcome/left in groups
+global.anticall = true //bot blocks user when called
+global.autoswview = true //auto status/story view
+global.adminevent = true //show promote/demote message
+global.groupevent = true //show update messages in group chat
 
 //Global messages
 global.mess = {
@@ -48,7 +48,7 @@ global.mess = {
 
 //text bug
 global.xbugtex = {
-xtxt: '👨🏼‍💻ELITE-PRO-V2👨🏼‍💻',
+xtxt: '👨🏼‍💻SYSSOLUTIONS-PRO-V2👨🏼‍💻',
 }
 
 
